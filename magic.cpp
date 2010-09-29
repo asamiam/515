51d1e29d0892575d724584d6f0be37d299b35d29
Ace Sorensen <asaelh@gmail.com>
2010-09-29 16:08:48 -0700

    This file converts images to greyscale and writes as .pgm... Not sure if it's totally right

---
+++ magic.cpp
@@ -0,0 +1,49 @@
+/*
+ *  magic.cpp
+ *  
+ *
+ *  Created by Ace Sorensen on 9/16/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+#include <Magick++.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace Magick;
+
+int main(int argc, char * argv[])
+{
+	cout << "CSE 515 Project 1 - Part 1" << endl;
+
+	if(argc != 2)
+	{
+		cout << "Error parsing command line arguments." << endl;
+		cout << "Usage: \nsegement <image-name>";
+		return 1;
+	}
+
+	// Get imageName
+	char *imageName;
+	imageName = argv[1];
+	cout << "ImageName: " << imageName << endl;
+
+	InitializeMagick(*argv);
+	
+	try
+	{
+		Image image(imageName);
+		image.quantizeColorSpace(GRAYColorspace);
+		image.write("test.pgm");
+		
+	}
+	catch(Magick::Exception &error_)
+	{
+		cout << "Error, image not loaded";
+		cout << error_.what();
+	}
+	return 0;
+}
+
+
